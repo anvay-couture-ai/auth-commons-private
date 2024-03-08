255bb251cd562425e2f2553f657a709797eee8be
anvay-couture-ai <anvay.karmore@couture.ai>
2024-03-08 12:32:45 +0530

    Change in middleware

--- src/middlewares.py
+++ src/middlewares.py
@@ -7,9 +7,10 @@ from .dependencies import UserDependency
 # from src.dependencies import get_user_from_token
 class AuthenticationMiddleware(BaseHTTPMiddleware):
 
-    def __init__(self, app, user_dependency):
+    def __init__(self, app, user_dependency, include_routes = None):
         super().__init__(app)
         self.user_dependency = user_dependency
+        self.include_routes = include_routes
         # Dictionary to store request counts for each IP
         # self.request_counts = {}
 
@@ -19,8 +20,6 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
         #print request body
 
         # get request headers
-        # breakpoint()
-
         if request.scope['path'] == '/auth/token' or request.scope['path'] == '/docs' or request.scope['path'] == '/redoc' or request.scope['path'] == '/openapi.json' or request.scope['path'] == '/favicon.ico' or request.scope['path'] == '/':
             return await call_next(request)
         try:
@@ -32,9 +31,13 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
             if not user:
                 return JSONResponse(status_code=401, content={"message": "Invalid token"})
         except IndexError:
-            return JSONResponse(status_code=401, content={"message": "Invalid token"})
-
+            if request.scope['path'] in self.include_routes:
+                return JSONResponse(status_code=401, content={"message": "Invalid token"})
+            request.state.authorized = False
+            response = await call_next(request)
+            return response
 
         request.state.user = user
+        request.state.authorized = True
         response = await call_next(request)
         return response
